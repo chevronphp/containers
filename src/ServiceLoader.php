10e7d10053dfaa9ebb6642a61e40c71f9d6927be
henderjon <jhenderson@capstonedigital.com>
2014-08-22 13:19:16 -0500

    guts ServiceLoader in favor of chevron.objectloader but maintains backwards compatability

---
+++ src/ServiceLoader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Chevron\Containers;
+
+use \Chevron\ObjectLoader\ObjectLoader;
+/**
+ * A recursive service loader to parse a directory (recursivley) and load in PHP
+ * file found. If the file returns a callable, it is called and passed a Deferred
+ * container. Essentially, this class creates a Di, and loads it from a number of
+ * service files. Each file should look like:
+ *
+ * <?php
+ * return function($di){
+ *     $di->set("nameOne", $value);
+ *     $di->set("nameTwo", function() use ($di){});
+ * }
+ *
+ * $value can be a callback (for singleton connections etc.)
+ *
+ * @package Chevron\Container
+ */
+class ServiceLoader {
+
+	function loadServices($path){
+
+		return (new ObjectLoader)->loadObject(new Di, $path);
+
+	}
+
+	function loadDi($path){
+		return $this->loadServices($path);
+	}
+
+}
